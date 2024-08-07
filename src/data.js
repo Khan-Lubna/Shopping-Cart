d24426187f9129dbea0d7e2a14211855dbe6499b
Khan-Lubna <116883265+Khan-Lubna@users.noreply.github.com>
2024-08-07 18:36:53 +0530

    Add files via upload

---
+++ src/data.js
@@ -0,0 +1,84 @@
+let shopItemsData = [{
+    id:"abc",
+    name:"Casual Shirt",
+    price: 45,
+    desc: "Elevate your everyday style with our Classic Casual Shirt.",
+    img: "./images/img-1.jpg"
+},
+{
+    id:"def",
+    name:"Office Shirt",
+    price: 100,
+    desc: "Our Classic White Dress Shirt is a timeless staple for any professional wardrobe.",
+    img: "./images/img-2.jpg"
+},
+{
+    id:"ghi",
+    name:"T-Shirt",
+    price: 25,
+    desc: "Stay cool and stylish with our moisture-wicking t-shirt, available in multiple colors.",
+    img: "./images/img-3.jpg"},
+{
+    id:"jkl",
+    name:"Mens Suit",
+    price: 300,
+    desc: "Embrace elegance with our Classic Fit Suit, crafted for the modern gentleman.",
+    img: "./images/img-4.jpg"
+},
+{
+    id:"mno",
+    name:"Mens Tie",
+    price: 25,
+    desc: "Add a touch of elegance to your wardrobe with our premium men's ties.Crafted from premium fabrics.",
+    img: "./images/img-5.png"  
+},
+{
+    id:"pqr",
+    name:"Casual Shoes",
+    price: 200,
+    desc: "Designed for everyday wear, these shoes offer a versatile look that complements any outfit",
+    img: "./images/img-6.png"  
+},
+{
+    id:"GHI",
+    name:"Black Suit",
+    price: 450,
+    desc: "Perfect for any occasion, this suit is tailored to perfection, ensuring a sleek and flattering fit.",
+    img: "./images/img-7.png"  
+},
+{
+    id:"stu",
+    name:"Polo Shirt",
+    price: 45,
+    desc: "Crafted from premium breathable cotton, this polo offers a comfortable fit and exceptional durability.",
+    img: "./images/img-8.png"  
+},
+{
+    id:"vwx",
+    name:"Denim Shirt",
+    price: 85,
+    desc: "Our Classic Denim Shirt combines the rugged appeal of denim with modern tailoring.",
+    img: "./images/img-9.png"  
+},
+{
+    id:"yz",
+    name:"Denim Pants",
+    price: 120,
+    desc: "Crafted from premium quality denim, these pants offer both comfort and durability.",
+    img: "./images/img-10.png"  
+},
+{
+    id:"ABC",
+    name:"Basic Cap",
+    price: 35,
+    desc: "Elevate your everyday style with our Classic Basic Cap, designed for comfort and versatility",
+    img: "./images/img-11.png"  
+},
+{
+    id:"DEF",
+    name:"Leather Boots",
+    price: 350,
+    desc: "Enhance your footwear collection with our Premium Leather Boots, where luxury meets functionality.",
+    img: "./images/img-12.png"  
+},
+];
